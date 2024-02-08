26d678324f267857fbfabe785ba45ca871815b34
sgurrola <124643885+sgurrola@users.noreply.github.com>
2024-02-08 15:06:34 -0600

    Horn stuff testing

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-//teehee
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", function (req, res) {
+    res.send("Hello World!");
+});
+
+app.get("/unique", function (req, res) {
+    res.send("This is a diff page!");
+});
+
+app.listen(port, function () {
+    console.log('Example app listening on port ${port}!');
+});
