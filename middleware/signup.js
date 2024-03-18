9815f8e019b1f67de9d9112529f50c32ccff5ced
Daisy Nieto <daisynieto@Daisys-MacBook-Air.local>
2024-03-18 14:35:55 -0500

    signup page started

---
+++ middleware/signup.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { insertUser } = require('./database');
+
+
+
+
+function handleSignup(req,res){
+    const {username,password,firstname,lastname,addressline,addressline2,city,state,zip } = req.body; // Assuming username and password are sent in the request body
+
+    insertUser(username,password,firstname,lastname,addressline, addressline2, city,state,zip, (err,userId) => {
+        if (err){
+            console.error('Error inserting: user', err);
+        }
+        else{
+            console.log('User inserted with ID:', userId);
+            res.render('home');
+        }
+    });
+
+
+}
+
+
+module.exports = {handleSignup};
+
