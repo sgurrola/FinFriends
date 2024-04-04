7ae817c8a77d3a40355c149e36fb28d541a537c7
rsoto-trinity <78444860+rsoto-trinity@users.noreply.github.com>
2024-04-04 11:25:52 -0500

    messages for login/signup and rm button for cart no func

--- middleware/signup.js
+++ middleware/signup.js
@@ -2,14 +2,14 @@ const express = require('express');
 const { insertUser,userExists } = require('./database');
 
 
-
-
 function handleSignup(req,res){
     const {username,password,firstname,lastname,addressline,addressline2,city,state,zip } = req.body; // Assuming username and password are sent in the request body
-
     userExists(username, (err, exists) => {
         if(err){
             console.error('error checking user', err);
+        }
+        if (!username || !password || !firstname || !lastname || !addressline || !city || !state || !zip) {
+            return res.render('signup', { error: 'All fields are required' });
         }
         if(exists) {
             res.status(400).json({ error: 'Username is already taken' }); // Send JSON response indicating that the username is taken
